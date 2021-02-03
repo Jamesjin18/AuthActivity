16b10a248398c880c16d00b34457c1a0bf6d961a
James <james.jin@efrei.net>
2021-02-03 17:47:12 +0100

    exo 1-10 (without comment)

--- app/src/main/java/edu/mda/authactivity/MainActivity.java
+++ app/src/main/java/edu/mda/authactivity/MainActivity.java
@@ -3,9 +3,15 @@ package edu.mda.authactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -24,25 +30,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.authenticate);
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+        final TextView result = (TextView) findViewById(R.id.result);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                URL url = null;
-                try {
-                    url = new URL("https://www.android.com/");
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                    try {
-                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                        String s = readStream(in);
-                        Log.i("JFL", s);
-                    } finally {
-                        urlConnection.disconnect();
+                new Thread(new Runnable(){
+                    public void run(){
+                        URL url = null;
+
+                        try {
+
+                            url = new URL("https://httpbin.org/basic-auth/bob/sympa");
+                            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                            try {
+
+                                String basicAuth = "Basic " + Base64.encodeToString((username.getText()+":"+password.getText()).getBytes(),Base64.NO_WRAP);
+                                urlConnection.setRequestProperty ("Authorization", basicAuth);
+                                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                                String s = readStream(in);
+                                Log.i("JFL", s);
+                                final JSONObject res = new JSONObject(s);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        if (true){
+                                            result.setText("res");
+                                        }
+
+
+                                    }
+                                });
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } finally {
+                                urlConnection.disconnect();
+                            }
+                        } catch (MalformedURLException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                }).start();
+
             }
         });
     }

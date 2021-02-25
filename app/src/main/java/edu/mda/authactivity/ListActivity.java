0b463fe41c11555f22fc7f599f8f235a1dab6506
James <james.jin@efrei.net>
2021-02-25 16:44:56 +0100

    exo20

--- app/src/main/java/edu/mda/authactivity/ListActivity.java
+++ app/src/main/java/edu/mda/authactivity/ListActivity.java
@@ -3,12 +3,42 @@ package edu.mda.authactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        ListView lv = (ListView) findViewById(R.id.list);
+        MyAdapter adapter = new MyAdapter();
+        lv.setAdapter(adapter);
+
+    }
+    public class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
     }
 }
